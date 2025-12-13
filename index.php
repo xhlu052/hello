83f13d0d1d9698ff7df9d4a16aa74279a0af9205
luca <scarssuper@gmail.com>
2025-12-14 00:31:31 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+
+<?php
+// This is a single-line comment
+echo "<p>Hello World</p>"; // 'echo' is used to output text to the browser
+?>
+
+</body>
+</html>
